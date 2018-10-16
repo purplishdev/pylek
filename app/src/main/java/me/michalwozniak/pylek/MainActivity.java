035843b2717a860df5f43a62d016bfca25a4c182
Michał Woźniak <40491049+purplishdev@users.noreply.github.com>
2018-10-16 20:31:53 +0200

    Added Retrofit and Butterknife libraries

--- app/src/main/java/me/michalwozniak/pylek/MainActivity.java
+++ app/src/main/java/me/michalwozniak/pylek/MainActivity.java
@@ -3,11 +3,14 @@ package me.michalwozniak.pylek;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
